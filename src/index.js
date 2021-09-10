b1be8723c97c974bad30f1b59ce3db8796a69443
Eduard Gavrila <edygavrila@gmail.com>
2021-09-11 00:04:10 +0100

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./style.css";
+const title1 = document.createElement("h1");
+title1.innerText = "Hello";
+title1.classList.add("hello");
+
+console.log(document.body);
+
+document.body.appendChild(title1);
